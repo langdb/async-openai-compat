d590376d8b81129ea1d2b62d1eb0b9b9df63ae71
Karolis Gudiškis <karolisgud@gmail.com>
2025-09-12 14:20:22 +0300

    fix: Update FinishReason enum to include an empty alias for 'eos'

--- async-openai/src/types/chat/chat_.rs
+++ async-openai/src/types/chat/chat_.rs
@@ -1053,7 +1053,7 @@ pub struct ChatCompletionStreamOptions {
 #[derive(Debug, Serialize, Deserialize, Clone, Copy, PartialEq)]
 #[serde(rename_all = "snake_case")]
 pub enum FinishReason {
-    #[serde(alias = "eos")]
+    #[serde(alias = "eos", alias = "")]
     Stop,
     Length,
     ToolCalls,
